cf47fdd9c7ec47eaa5359d328ceebcda2faa1856
Ilgiz <ilgizn395@gmail.com>
2022-12-12 10:50:38 +0300

    delete comments

--- parser/browser.py
+++ parser/browser.py
@@ -1,6 +1,3 @@
-# import asyncio
-# from playwright.async_api import async_playwright
-# from playwright.sync_api import sync_playwright
 import json
 from selenium.webdriver.common.by import By
 from selenium import webdriver
@@ -12,10 +9,6 @@ def main():
         for page_num in range(1, 101):
             print("page",page_num)
             page.get(f'https://www.avito.ru/kazan/kvartiry/prodam-ASgBAgICAUSSA8YQ?cd=1&p={page_num}')
-            # print("wait element !!!!!!!!")
-            # page.wait_for_selector('div[data-marker="item"]')
-            # print("elemet find!!!!!!!")
-
             links = page.find_elements(By.XPATH, '//*[@data-marker="item"]/div/div[2]/div[2]/a')
             for item in links:
                 parsed.append(
